70ecfc54b7cc43c84da53cd84889d99f3b604d13
satoshi aoki <satozi.ak.3250@gmail.com>
2021-10-15 15:42:06 +0900

    トップ画面　投稿機能生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  devise_for :users
+  resources :articles
+  root to: 'tops#index'
+  get "users/show" => "users#show"
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
